8001ccaa51fa2f5cd01132ba034d5c319cd7163e
Staroyee <danmasefield@gmail.com>
2023-11-15 21:19:21 +1030

    Finalised color scheme

--- client/src/components/Countdown.jsx
+++ client/src/components/Countdown.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { PropTypes } from "prop-types";
 import { Row, Col } from "react-bootstrap";
-import "../assets/css/Countdown.css";
 
 const CountdownTimer = ({ targetDate }) => {
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,11 +6,11 @@ function Home() {
     <>
       <Container className="homeContainer">
         <Row>
-          <Col xs={6} className="homeColumn">
+          <Col md={12} lg={6} className="homeColumn">
             <h1 className="title">SKYWARD</h1>
             <h3 className="info">Look to the sky and view the galaxy, track space launches, browse rocket information and more!</h3>
           </Col>
-          <Col xs={6}></Col>
+          <Col md={12} lg={6}></Col>
         </Row>
       </Container>
     </>
