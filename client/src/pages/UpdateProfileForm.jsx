fae6b9c653f5891abfb30605eea5c66d9c24096e
Staroyee <danmasefield@gmail.com>
2023-11-18 18:04:42 +1030

    Styled UpdateProfileForm

--- client/src/pages/UpdateProfileForm.jsx
+++ client/src/pages/UpdateProfileForm.jsx
@@ -1,46 +1,72 @@
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { UPDATE_PROFILE } from '../utils/mutations';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { UPDATE_PROFILE } from "../utils/mutations";
+
+import { Container, Row, Col, Form } from "react-bootstrap";
+import "../assets/css/UpdateProfileForm.css"
 
 const UpdateProfileForm = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [updateProfile] = useMutation(UPDATE_PROFILE);
 
-  const handleUpdate = async (e) => {
-    e.preventDefault();
+  const handleUpdate = async () => {
     try {
       await updateProfile({
         variables: { username, email, password },
       });
       // Optionally, you can fetch the updated profile data and update the UI
     } catch (error) {
-      console.error('Update failed:', error.message);
+      console.error("Update failed:", error.message);
     }
   };
 
   return (
-    <div>
-      <h2>Update Profile</h2>
-      <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      
-      <button onClick={handleUpdate}>Update Profile</button>
-      </form>
-    </div>
+    <>
+      <Container className="UPF-Container">
+        <Row>
+          <Col>
+            <h1 className="UPF-Title">Edit Details</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Form>
+            <Form.Group className="UPF-FormGroup">
+              <h5>Username:</h5>
+              <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="UPF-FormGroup">
+              <h5>Email:</h5>
+              <Form.Control
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="UPF-FormGroup">
+              <h5>Password:</h5>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Col className="UPF-ButtonContainer">
+              <Link to={"/profile"}>
+            <button className="UPF-Button" onClick={handleUpdate}>Update Profile</button>
+            </Link>
+            </Col>
+          </Form>
+        </Row>
+      </Container>
+    </>
   );
 };
 
