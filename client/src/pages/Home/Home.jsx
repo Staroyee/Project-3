7d273d79c0fd26d4738100e936876ff4de97eb7f
Staroyee <danmasefield@gmail.com>
2023-12-28 23:39:07 +1030

    Created nav button for homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import "../../assets/fonts/nasalization-rg.otf";
 import "./Home.css";
 
@@ -9,12 +11,37 @@ function Home() {
       <Container className="H-Container">
         <Row>
           <Col md={12} className="H-Column">
-          <svg height="100" stroke="#072448" strokeWidth="1.5" className="text-line" width="100%"><text x="50%" dominantBaseline="middle" textAnchor="middle" y="50%">SKYWARD</text><h1 className="H-Title">SKYWARD</h1></svg>
-            
+            <svg
+              height="100"
+              stroke="#072448"
+              strokeWidth="1.5"
+              className="text-line"
+              width="100%"
+            >
+              <text
+                x="50%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                y="50%"
+              >
+                SKYWARD
+              </text>
+              <h1 className="H-Title">SKYWARD</h1>
+            </svg>
+
             <h3 className="H-Info">
               Look to the sky and view the galaxy, track space launches, browse
               rocket information and more!
             </h3>
+            <Link to="/login-signup">
+              <motion.button
+                className="box H-Button"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 200, damping: 15 }}
+              >
+                Journey Skyward
+              </motion.button>
+            </Link>
           </Col>
           <Col md={12}></Col>
         </Row>
